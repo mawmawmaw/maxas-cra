252c8c24940fef14fb942079a50b684e6dd3aff5
Mauricio García de Ceca <shinji.256@gmail.com>
2022-08-08 17:58:15 -0500

    Added Collections info and images

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,5 +1,9 @@
 import './Collection.css';
 import icon from '../../assets/icon-collection.png';
+import img1 from '../../assets/collection-1.png';
+import img2 from '../../assets/collection-2.png';
+import img3 from '../../assets/collection-3.png';
+import img4 from '../../assets/collection-4.png';
 const Collection = () => {
     return (
         <div id="collection" className='section'>
@@ -8,8 +12,20 @@ const Collection = () => {
                 <div className='content grid'>
                     <img src={icon} className="section-icon" alt="Collection section icon" />
                     <div>
-                        <p>Vivamus sem arcu, gravida eget consectetur in, maximus euismod quam. Quisque pretium viverra purus. Curabitur consequat massa sit amet augue varius volutpat. Aenean maximus erat id dignissim semper. Pellentesque in ornare leo, a porttitor tellus. Nunc sit amet lectus sit amet nisl iaculis efficitur. Fusce fringilla odio a efficitur tincidunt. Mauris semper nunc et est pellentesque aliquam.</p>
+                        <p>As we hold you all so Dear we would love to share with you the legend of the blue deer that has guided us here...</p>
+                        <p>Many moons ago, tucked between the folds of the Huichol mountains in North Central Mexico, the Wixaritari Elders gathered in their high up villages. The atmosphere was heavy with heat and desperation: there was no food, no rain and the grounds were bone-dry. The Elders decided to send four young hunters to scavenge food for the community. Each hunter embodied one of the sacred elements: fire, water, air and earth.</p>
                     </div>
+                </div>
+                <div className='content'>
+                    <p>Armed with bows and arrows, the hunters walked for days. They were exhausted, dehy- drated and losing hope. Suddenly, a maxa (deer) sprung from the bushes, towering over them in all its splendor. The hunters began to chase it, leaving all their tiredness behind... Little did they know, the deer was compassionate, taking pity on the starving men, making their pursuit easier at times.</p>
+                    <p>After weeks of pursuit, they arrived at Wirikuta, the sacred grounds where the world was created and the spirit of the Earth resides. There, glistening like emeralds in the sun, was a large cluster of peyote cacti, formed in the shape of a deer. In his confusion, one of the hunters raised his bow and shot an arrow right into the heart of the plants. Taking this as a sign, the hunters brought the peyote back to their village, where the Elders divided them up amongst the people. Soon after, a miracle occurred, and the people of the town were all cured of their illnesses, hunger, and thirst.</p>
+                    <p>To this day, the Wixaritari people worship this very precious peyote medicine, holding the spirit of Kayumari, the blue deer, as their protector and guide.</p>
+                </div>
+                <div className='content grid gallery'>
+                    <img src={img1} alt='Example of Maxas NFT' />
+                    <img src={img2} alt='Example of Maxas NFT' />
+                    <img src={img3} alt='Example of Maxas NFT' />
+                    <img src={img4} alt='Example of Maxas NFT' />
                 </div>
             </div>
         </div>
